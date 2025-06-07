0b6249b965402c5f24c3795d8e6f15be65bcffbe
Stephane Georget <s_georget_2000@yahoo.fr>
2025-06-07 23:12:01 +0200

    Added sliders to sea & vortex

--- sea_n_vortex.hlsl
+++ sea_n_vortex.hlsl
@@ -1,7 +1,62 @@
-// rotating maze
+// Sea and Vortex
 // From Yohei Nishitsuji
 // https://x.com/YoheiNishitsuji/status/1931100986220945776
-// [SGEORGET20250607215735]
+// Sliders added by S. Georget
+// [SGEORGET20250607231136]
+
+uniform float _016_Speed<
+  string label = "Speed (0.2)";
+  string widget_type = "slider";
+  string group = "Adjustments";
+  float minimum = -2;
+  float maximum = 2;
+  float step = 0.01;
+> = 0.2;
+
+
+
+uniform float _016_ParamA<
+  string label = "Terrain resolution (12)";
+  string widget_type = "slider";
+  string group = "Adjustments";
+  float minimum = 1;
+  float maximum = 18;
+  float step = 1;
+> = 12;
+
+
+
+uniform int _016_ParamB<
+  string label = "Configuration (0)";
+  string widget_type = "slider";
+  string group = "Adjustments";
+  int minimum = 0;
+  int maximum = 5;
+  int step = 1;
+> = 0;
+
+
+uniform float _016_ParamC<
+  string label = "Shift up/down (1.0)";
+  string widget_type = "slider";
+  string group = "Adjustments";
+  float minimum = -1;
+  float maximum = 2;
+  float step = 0.01;
+> = 1.0;
+
+
+
+uniform float _016_ViewpointHeight<
+  string label = "Mountain Height D (3.0)";
+  string widget_type = "slider";
+  string group = "Adjustments";
+  float minimum = 2.1;
+  float maximum = 4;
+  float step = 0.01;
+> = 3.0;
+
+
 
 struct VertData {
     float4 pos : POSITION;
@@ -24,8 +79,31 @@ float3 hsv_safe(float h, float s, float v) {
 
 float4 mainImage(VertData v_in) : TARGET {
     float2 r = uv_size;
-    float2 FC = float2(v_in.uv.x * r.x, (1.0 - v_in.uv.y) * r.y);
-    float t = elapsed_time;
+    float2 FC;
+    switch(_016_ParamB)
+    {
+        case 1:
+        FC = float2(v_in.uv.y * r.y, (_016_ParamC - v_in.uv.x) * r.x);
+        break;
+
+        case 2:
+        FC = float2(v_in.uv.x * r.y, (_016_ParamC - v_in.uv.y) * r.x);
+        break;
+
+        case 3:
+        FC = float2(v_in.uv.x * r.y, (_016_ParamC - v_in.uv.y) * r.y);
+        break;
+    
+        case 4:
+        FC = float2(v_in.uv.x * r.x , (_016_ParamC - v_in.uv.y) * (r.y)  )/_016_ParamC;
+        break;
+
+        default:
+        FC = float2(v_in.uv.x * r.x, (_016_ParamC - v_in.uv.y) * r.y);
+        break;
+    }
+
+    float t = elapsed_time * _016_Speed;
 
     float i = 0., e = 0., g = 0., R = 1., s;
     float3 o = float3(0, 0, 0);
@@ -38,14 +116,14 @@ float4 mainImage(VertData v_in) : TARGET {
     for (; i++ < 100.; ) {
         e += i / 9e9;
         o += hsv_safe(0.1, q.y, min(e * i, 0.01));
-        s = 3.0;
+        s = _016_ViewpointHeight;
         p = q += d * e * R * 0.25;
         g += p.y / s;
         R = (length(p));
         p = float3(log2(R) + t * 0.2, exp2(fmod(-p.z, s) / R) - 0.3, p.x);
         e = p.y - 1.0;
-        for (; s < 6000.0; s += s) {
-            e += -abs(dot(sin(p.xz * s), cos(float2(p.z, p.y) * s)) / s * 0.4);
+        for (; s < pow(_016_ParamA, 3); s += s) {
+            e += -abs(dot(sin(p.xz * s), cos(float2((p.z), p.y) * s)) / s * 0.4);
         }
     }
 
